65e175abaeb06c023059901dd0a2483f39e7d9f4
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-07-22 15:02:26 +0200

    Add fixed variant

--- flake.nix
+++ flake.nix
@@ -18,38 +18,10 @@
             pkgs = nixpkgs.legacyPackages.${system};
 
 
-            plainPackage =
-              let
-                iosevkaWithWeb = pkgs.iosevka.overrideAttrs (finalAttrs: previousAttrs: {
-                  # Replace `ttf::$pname with `contents::$pname` in buildPhase to get webfont aswell
-                  # See https://typeof.net/Iosevka/customizer for more options
-                  buildPhase = ''
-                    export HOME=$TMPDIR
-                    runHook preBuild
-                    npm run build --no-update-notifier -- --jCmd=$NIX_BUILD_CORES contents::$pname
-                    runHook postBuild
-                  '';
-
-                  # Override installPhase to copy the addional woff2 and css files
-                  installPhase = ''
-                    runHook preInstall
-                    fontdir="$out/share/fonts"
-                    mkdir -p "$fontdir"
-                    cp -r "dist/$pname"/* "$fontdir"
-                    runHook postInstall
-                  '';
-                });
-              in
-              iosevkaWithWeb.override {
-                # Use https://typeof.net/Iosevka/customizer to generate build plan
-                privateBuildPlan = builtins.readFile ./iosevka-qp.toml;
-                set = "qp";
-              };
-
             # nerdFontPackage = let outDir = "$out/share/fonts/truetype/"; in
             # pkgs.stdenv.mkDerivation {
             # pname = "iosevka-qp-nerd-font";
-            # version = plainPackage.version;
+            # version =iosevka-qp.version;
 
             # src = builtins.path { path = ./.; name = "iosevka-qp"; };
 
@@ -57,7 +29,7 @@
 
             # configurePhase = "mkdir -p ${outDir}";
             # buildPhase = ''
-            #   for fontfile in ${plainPackage}/share/fonts/truetype/*
+            #   for fontfile in ${iosevka-qp}/share/fonts/truetype/*
             #   do
             #   nerd-font-patcher $fontfile --complete --careful --outputdir ${outDir}
             #   done
@@ -65,20 +37,57 @@
             # dontInstall = true;
             # };
 
-            packages = {
-              normal = plainPackage;
-              # nerd-font = nerdFontPackage;
-            };
           in
           {
-            inherit packages;
-            defaultPackage = plainPackage;
+            packages =
+              let
+                # Replace `ttf::$pname with `contents::$pname` in buildPhase to get webfont aswell
+                # See https://typeof.net/Iosevka/customizer for more options
+                buildPhase = ''
+                  export HOME=$TMPDIR
+                  runHook preBuild
+                  npm run build --no-update-notifier -- --jCmd=$NIX_BUILD_CORES contents::$pname
+                  runHook postBuild
+                '';
+
+                # Override installPhase to copy the addional woff2 and css files
+                installPhase = ''
+                  runHook preInstall
+                  fontdir="$out/share/fonts"
+                  mkdir -p "$fontdir"
+                  cp -r "dist/$pname"/* "$fontdir"
+                  runHook postInstall
+                '';
+
+              in
+
+              # Use https://typeof.net/Iosevka/customizer to generate build plan
+              rec {
+                iosevka-qp = (pkgs.iosevka.overrideAttrs (finalAttrs: previousAttrs: {
+                  inherit buildPhase installPhase;
+                })).override {
+                  privateBuildPlan = builtins.readFile ./iosevka-qp.toml;
+                  set = "qp";
+                };
+
+                iosevka-fixed = pkgs.iosevka.overrideAttrs (finalAttrs: previousAttrs: {
+                  inherit buildPhase installPhase;
+                });
+
+                # iosevka-fixed = (pkgs.iosevka.overrideAttrs (finalAttrs: previousAttrs: {
+                #   inherit buildPhase installPhase;
+                # })).override {
+                #   privateBuildPlan = builtins.readFile ./iosevka-fixed.toml;
+                #   set = "fixed";
+                # };
+                default = iosevka-fixed;
+                # nerd-font = nerdFontPackage;
+              };
           }
         );
     in
     {
       packages = allSystems.packages;
-      defaultPackage = allSystems.defaultPackage;
       overlay = final: prev: {
         iosevka-qp = allSystems.packages.${final.system}; # either `normal` or `nerd-font`
       };
